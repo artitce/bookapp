07c61d16a149e2ba8d944078e986e9b223467fba
artitce <arti.manputra@tce.co.in>
2024-09-17 16:16:17 +0530

    added all the files

---
+++ src/components/BookList.js
@@ -0,0 +1,17 @@
+import React,{useState} from 'react'
+import BookShow from './BookShow'
+
+const BookList = ({books,onDelete,onEdit}) => {
+    const rendernBooks=books.map((book)=>{
+        return(
+            <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />
+        )
+    })
+  return (
+    <div className='book-list'>
+      {rendernBooks}
+    </div>
+  )
+}
+
+export default BookList
